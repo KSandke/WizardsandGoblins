7d793498a713598a26f59f289e5c79362c607f04
Kevin <kevinsandke7@gmail.com>
2024-10-23 21:36:50 -0700

    Initial Commit

---
+++ WizardsandGoblins/WizardsandGoblinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WizardsandGoblinsApp.swift
+//  WizardsandGoblins
+//
+//  Created by Kevin Sandke on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct WizardsandGoblinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WizardsandGoblinsTests/WizardsandGoblinsTests.swift
@@ -0,0 +1,17 @@
+//
+//  WizardsandGoblinsTests.swift
+//  WizardsandGoblinsTests
+//
+//  Created by Kevin Sandke on 10/23/24.
+//
+
+import Testing
+@testable import WizardsandGoblins
+
+struct WizardsandGoblinsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
