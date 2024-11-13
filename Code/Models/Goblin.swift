874188ce75ed4e50eb3604e168d7e48afaec3ff1
GeorgeZipp <aliensareblue@gmail.com>
2024-11-13 14:06:31 -0800

    Revert "New goblin types and wave"
    This reverts commit 92a0e99930803bcb36ca2fe116d7b644ce92ef31.

--- Code/Models/Goblin.swift
+++ Code/Models/Goblin.swift
@@ -1,7 +1,7 @@
 import Foundation
 import SpriteKit
 
-class GoblinManager {
+class Goblin {
     enum GoblinType {
         case normal
         case large
@@ -10,15 +10,13 @@ class GoblinManager {
     
     class GoblinContainer {
         let sprite: SKSpriteNode
-        let type: GoblinType
         let healthBar: SKShapeNode
         let healthFill: SKShapeNode
         var health: CGFloat
         let damage: CGFloat
         let maxHealth: CGFloat 
                 
-        init(type: GoblinType, sprite: SKSpriteNode, healthBar: SKShapeNode, healthFill: SKShapeNode, health: CGFloat, damage: CGFloat, maxHealth: CGFloat) {
-            self.type = type
+        init(sprite: SKSpriteNode, healthBar: SKShapeNode, healthFill: SKShapeNode, health: CGFloat, damage: CGFloat, maxHealth: CGFloat) {
             self.sprite = sprite
             self.healthBar = healthBar
             self.healthFill = healthFill
@@ -28,36 +26,30 @@ class GoblinManager {
         }
     }
     
+    let type: GoblinType
     weak var scene: SKScene?
     var goblinContainers: [GoblinContainer] = []
     
-    // Probabilities for each goblin type
-    var goblinTypeProbabilities: [GoblinType: Double]
-    
-    init(scene: SKScene, probabilities: [GoblinType: Double]) {
+    init(type: GoblinType = .normal, scene: SKScene) {
+        self.type = type
         self.scene = scene
-        self.goblinTypeProbabilities = probabilities
     }
-
+    
     func spawnGoblin(at position: CGPoint) {
         guard let scene = scene else { return }
         
-        // Decide which goblin type to spawn based on probabilities
-        let goblinType = randomGoblinType()
-        
-        // Create a goblin of that type
-        let goblinSprite = SKSpriteNode(imageNamed: imageName(for: goblinType))
-        goblinSprite.size = goblinSize(for: goblinType)
-        goblinSprite.position = position
-        goblinSprite.name = "goblin"
+        let goblin = SKSpriteNode(imageNamed: imageName())
+        goblin.size = goblinSize()
+        goblin.position = position
+        goblin.name = "goblin"
         
         // Create health bar background
-        let healthBarWidth: CGFloat = goblinSprite.size.width * 0.8
+        let healthBarWidth: CGFloat = goblin.size.width * 0.8
         let healthBarHeight: CGFloat = 5
         let healthBar = SKShapeNode(rectOf: CGSize(width: healthBarWidth, height: healthBarHeight))
         healthBar.fillColor = .gray
         healthBar.strokeColor = .black
-        healthBar.position = CGPoint(x: 0, y: goblinSprite.size.height / 2 + 5)
+        healthBar.position = CGPoint(x: 0, y: goblin.size.height/2 + 5)
         
         // Create health bar fill
         let healthFill = SKShapeNode(rectOf: CGSize(width: healthBarWidth, height: healthBarHeight))
@@ -66,22 +58,21 @@ class GoblinManager {
         healthFill.position = healthBar.position
         
         // Add health bars as children of goblin
-        goblinSprite.addChild(healthBar)
-        goblinSprite.addChild(healthFill)
+        goblin.addChild(healthBar)
+        goblin.addChild(healthFill)
         
-        let physicsBody = SKPhysicsBody(rectangleOf: goblinSprite.size)
+        let physicsBody = SKPhysicsBody(rectangleOf: goblin.size)
         physicsBody.isDynamic = false
         physicsBody.affectedByGravity = false
         physicsBody.allowsRotation = false
         physicsBody.categoryBitMask = 1
         physicsBody.contactTestBitMask = 2
-        goblinSprite.physicsBody = physicsBody
+        goblin.physicsBody = physicsBody
         
-        let health = goblinHealth(for: goblinType)
-        let damage = goblinDamage(for: goblinType)
+        let health = goblinHealth()
+        let damage = goblinDamage()
         let container = GoblinContainer(
-            type: goblinType,
-            sprite: goblinSprite,
+            sprite: goblin,
             healthBar: healthBar,
             healthFill: healthFill,
             health: health,
@@ -90,31 +81,15 @@ class GoblinManager {
         )
         goblinContainers.append(container)
         
-        scene.addChild(goblinSprite)
+        scene.addChild(goblin)
         
         moveGoblin(container: container)
-    }
-    
-    private func randomGoblinType() -> GoblinType {
-        // Compute total probability
-        let totalProbability = goblinTypeProbabilities.values.reduce(0, +)
-        // Generate random number between 0 and totalProbability
-        let randomValue = Double.random(in: 0..<totalProbability)
-        var cumulativeProbability = 0.0
-        for (type, probability) in goblinTypeProbabilities {
-            cumulativeProbability += probability
-            if randomValue < cumulativeProbability {
-                return type
-            }
-        }
-        // Default to normal if something goes wrong
-        return .normal
     }
     
     private func moveGoblin(container: GoblinContainer) {
         guard let scene = scene as? GameScene else { return }
         let targetPosition = scene.playerView.castlePosition
-        let moveDuration = TimeInterval(container.sprite.position.distance(to: targetPosition) / goblinSpeed(for: container.type))
+        let moveDuration = TimeInterval(container.sprite.position.distance(to: targetPosition) / goblinSpeed())
         
         let moveAction = SKAction.move(to: targetPosition, duration: moveDuration)
         let damageAction = SKAction.run { [weak self] in
@@ -127,7 +102,12 @@ class GoblinManager {
         container.sprite.run(sequence)
     }
     
-    func goblinSpeed(for type: GoblinType) -> CGFloat {
+    func removeGoblin(container: GoblinContainer) {
+        goblinContainers.removeAll { $0.sprite == container.sprite }
+        container.sprite.removeFromParent()
+    }
+    
+    func goblinSpeed() -> CGFloat {
         switch type {
         case .normal:
             return 100
@@ -138,7 +118,7 @@ class GoblinManager {
         }
     }
     
-    func goblinHealth(for type: GoblinType) -> CGFloat {
+    func goblinHealth() -> CGFloat {
         switch type {
         case .normal:
             return 50
@@ -149,7 +129,7 @@ class GoblinManager {
         }
     }
     
-    func goblinDamage(for type: GoblinType) -> CGFloat {
+    func goblinDamage() -> CGFloat {
         switch type {
         case .normal:
             return 10
@@ -160,7 +140,7 @@ class GoblinManager {
         }
     }
     
-    func goblinSize(for type: GoblinType) -> CGSize {
+    func goblinSize() -> CGSize {
         switch type {
         case .normal:
             return CGSize(width: 50, height: 50)
@@ -171,14 +151,14 @@ class GoblinManager {
         }
     }
     
-    func imageName(for type: GoblinType) -> String {
+    func imageName() -> String {
         switch type {
         case .normal:
             return "Goblin1"
         case .large:
-            return "Goblin1"   // Update with the appropriate image asset
+            return "Goblin1"   // Add appropriate image assets
         case .small:
-            return "Goblin1"   // Update with the appropriate image asset
+            return "Goblin1"   // Add appropriate image assets
         }
     }
     
@@ -210,11 +190,6 @@ class GoblinManager {
         }
     }
 
-    func removeGoblin(container: GoblinContainer) {
-        goblinContainers.removeAll { $0 === container }
-        container.sprite.removeFromParent()
-    }
-    
     func removeAllGoblins(in gameScene: GameScene) {
         var containersToRemove: [GoblinContainer] = []
         for container in goblinContainers {
--- Code/Scenes/WGGameScene.swift
+++ Code/Scenes/WGGameScene.swift
@@ -19,8 +19,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     // Background
     var background: SKSpriteNode!
     
-    // Goblin Manager
-    var goblinManager: GoblinManager!
+    // Goblin Managers
+    var normalGoblinManager: Goblin!
+    var largeGoblinManager: Goblin!
+    var smallGoblinManager: Goblin!
     let goblinSpawnInterval: TimeInterval = 2.0
     
     // Mana potions
@@ -57,13 +59,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         physicsWorld.gravity = .zero
         physicsWorld.contactDelegate = self
         
-        // Initialize Goblin Manager with probabilities
-        let goblinProbabilities: [GoblinManager.GoblinType: Double] = [
-            .normal: 70.0,
-            .large: 15.0,
-            .small: 15.0
-        ]
-        goblinManager = GoblinManager(scene: self, probabilities: goblinProbabilities)
+        // Initialize Goblin Managers
+        normalGoblinManager = Goblin(type: .normal, scene: self)
+        largeGoblinManager = Goblin(type: .large, scene: self)
+        smallGoblinManager = Goblin(type: .small, scene: self)
         
         // Start the first wave
         startWave()
@@ -113,24 +112,139 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.run(repeatSpawnGoblin, withKey: "spawnGoblin")
     }
     
+    func endWave() {
+        // Stop the actions
+        self.removeAction(forKey: "regenerateMana")
+        self.removeAction(forKey: "spawnPotion")
+        self.removeAction(forKey: "spawnGoblin")
+        
+        isSpawningEnabled = false
+    }
+    
+    func spawnManaPotion() {
+        // Only spawn if the wave is active
+        guard isSpawningEnabled else { return }
+        
+        // Create random position within playable area
+        let randomX = CGFloat.random(in: 100...size.width-100)
+        let randomY = CGFloat.random(in: 200...size.height-100)
+        let position = CGPoint(x: randomX, y: randomY)
+        
+        // Create mana potion sprite
+        let potion = SKSpriteNode(color: .blue, size: CGSize(width: 30, height: 30))
+        potion.position = position
+        potion.name = "manaPotion"
+        
+        // Add some visual effects
+        potion.alpha = 0.8
+        let pulse = SKAction.sequence([
+            SKAction.scale(to: 1.2, duration: 0.5),
+            SKAction.scale(to: 1.0, duration: 0.5)
+        ])
+        potion.run(SKAction.repeatForever(pulse))
+        
+        // Add automatic removal after duration
+        let removeSequence = SKAction.sequence([
+            SKAction.wait(forDuration: manaPotionDuration),
+            SKAction.fadeOut(withDuration: 1.0),
+            SKAction.removeFromParent()
+        ])
+        potion.run(removeSequence)
+        
+        addChild(potion)
+        manaPotions.append(potion)
+    }
+    
+    func setupBackground() {
+        background = SKSpriteNode(imageNamed: "Background")
+        background.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        background.size = self.size
+        background.zPosition = -1
+        addChild(background)
+    }
+    
+    func handlePotionHit(potion: SKSpriteNode, spellLocation: CGPoint) {
+        // Find the closest wizard to the spell location
+        let distanceToPlayerOne = spellLocation.distance(to: playerView.playerOnePosition)
+        let distanceToPlayerTwo = spellLocation.distance(to: playerView.playerTwoPosition)
+        
+        // Determine which wizard gets the mana
+        if distanceToPlayerOne < distanceToPlayerTwo {
+            playerState.playerOneMana = min(playerState.maxMana, playerState.playerOneMana + manaPotionManaRestore)
+        } else {
+            playerState.playerTwoMana = min(playerState.maxMana, playerState.playerTwoMana + manaPotionManaRestore)
+        }
+        
+        // Create mana restore effect
+        createManaRestoreEffect(at: potion.position)
+        
+        // Remove the potion
+        if let index = manaPotions.firstIndex(of: potion) {
+            manaPotions.remove(at: index)
+        }
+        potion.removeFromParent()
+    }
+    
+    func createManaRestoreEffect(at position: CGPoint) {
+        // Create a visual effect for mana restoration
+        let effect = SKEmitterNode()
+        effect.particleTexture = SKTexture(imageNamed: "spark") // Add a spark image to assets
+        effect.position = position
+        effect.particleBirthRate = 100
+        effect.numParticlesToEmit = 50
+        effect.particleLifetime = 0.5
+        effect.particleColor = .blue
+        effect.particleColorBlendFactor = 1.0
+        effect.particleScale = 0.5
+        effect.particleScaleSpeed = -1.0
+        effect.emissionAngle = 0.0
+        effect.emissionAngleRange = .pi * 2
+        effect.particleSpeed = 100
+        effect.xAcceleration = 0
+        effect.yAcceleration = 0
+        addChild(effect)
+        
+        // Remove effect after duration
+        let wait = SKAction.wait(forDuration: 0.5)
+        let remove = SKAction.removeFromParent()
+        effect.run(SKAction.sequence([wait, remove]))
+    }
+    
+    func createCoinEffect(at position: CGPoint) {
+        let coinSprite = SKSpriteNode(imageNamed: "coin") // Make sure to add a coin image to assets
+        coinSprite.size = CGSize(width: 20, height: 20)
+        coinSprite.position = position
+        addChild(coinSprite)
+        
+        let moveUp = SKAction.moveBy(x: 0, y: 50, duration: 0.5)
+        let fade = SKAction.fadeOut(withDuration: 0.3)
+        let remove = SKAction.removeFromParent()
+        let sequence = SKAction.sequence([moveUp, fade, remove])
+        
+        coinSprite.run(sequence)
+    }
+    
     func spawnGoblin(at position: CGPoint) {
-        guard isSpawningEnabled, totalGoblinsSpawned < maxGoblinsPerWave, !isGameOver else {
+        if !isSpawningEnabled || totalGoblinsSpawned >= maxGoblinsPerWave || isGameOver {
             return
         }
         
         totalGoblinsSpawned += 1
-        goblinManager.spawnGoblin(at: position)
+        
+        // Randomly decide which type of goblin to spawn
+        let goblinTypeChance = Int.random(in: 1...100)
+        if goblinTypeChance <= 70 {
+            normalGoblinManager.spawnGoblin(at: position)
+        } else if goblinTypeChance <= 85 {
+            largeGoblinManager.spawnGoblin(at: position)
+        } else {
+            smallGoblinManager.spawnGoblin(at: position)
+        }
     }
     
-    func applySpell(_ spell: Spell, at position: CGPoint) {
-        // Apply spell effects to goblins
-        goblinManager.applySpell(spell, at: position, in: self)
-        
-        // Check for potion hits
-        for potion in manaPotions {
-            if position.distance(to: potion.position) <= spell.aoeRadius {
-                handlePotionHit(potion: potion, spellLocation: position)
-            }
+    func castleTakeDamage(damage: CGFloat) {
+        if playerState.takeDamage(damage) {
+            gameOver()
         }
     }
     
@@ -141,14 +255,175 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         isGameOver = true
         
         // Remove any remaining goblins and potions
-        goblinManager.removeAllGoblins(in: self)
+        normalGoblinManager.removeAllGoblins(in: self)
+        largeGoblinManager.removeAllGoblins(in: self)
+        smallGoblinManager.removeAllGoblins(in: self)
         
         for potion in manaPotions {
             potion.removeFromParent()
         }
         manaPotions.removeAll()
         
-        // Rest of your game over logic...
+        
+        let gameOverLabel = SKLabelNode(text: "Game Over!")
+        gameOverLabel.fontSize = 50
+        gameOverLabel.fontColor = .red
+        gameOverLabel.position = CGPoint(x: size.width / 2, y: size.height * 0.7)
+        addChild(gameOverLabel)
+        // Remove all nodes and reset the scene
+        removeAllChildren()
+        
+        // Add final score label
+        let finalScoreLabel = SKLabelNode(text: "Final Score: \(playerState.score)")
+        finalScoreLabel.fontSize = 40
+        finalScoreLabel.fontColor = .white
+        finalScoreLabel.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        addChild(finalScoreLabel)
+        
+        // Add final coins label
+        let finalCoinsLabel = SKLabelNode(text: "Total Coins: \(playerState.coins)")
+        finalCoinsLabel.fontSize = 40
+        finalCoinsLabel.fontColor = .yellow
+        finalCoinsLabel.position = CGPoint(x: size.width / 2, y: size.height * 0.6)
+        addChild(finalCoinsLabel)
+        
+        // Add Restart Button
+        restartButton = SKLabelNode(text: "Restart")
+        restartButton.fontSize = 30
+        restartButton.fontColor = .white
+        restartButton.position = CGPoint(x: size.width / 2, y: size.height / 2 - 50)
+        restartButton.name = "restartButton"
+        addChild(restartButton)
+        
+        // Add Main Menu Button
+        mainMenuButton = SKLabelNode(text: "Main Menu")
+        mainMenuButton.fontSize = 30
+        mainMenuButton.fontColor = .white
+        mainMenuButton.position = CGPoint(x: size.width / 2, y: size.height / 2 - 100)
+        mainMenuButton.name = "mainMenuButton"
+        addChild(mainMenuButton)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let touchLocation = touch.location(in: self)
+        
+        if isInShop {
+            // Forward touch to shop view if it exists
+            if let shopView = self.children.first(where: { $0 is ShopView }) as? ShopView {
+                shopView.handleTap(at: touchLocation)
+            }
+            return
+        }
+        
+        // Handle button taps
+        let touchedNode = nodes(at: touchLocation).first  // Get the first node at touch location
+        if let name = touchedNode?.name {
+            switch name {
+            case "restartButton":
+                restartGame()
+                return
+            case "mainMenuButton":
+                goToMainMenu()
+                return
+            default:
+                break
+            }
+        }
+        
+        // Determine which wizard is casting based on proximity
+        let p1Position = playerView.playerOnePosition
+        let p2Position = playerView.playerTwoPosition
+        
+        // Calculate distances to each wizard
+        let distance1 = touchLocation.distance(to: p1Position)
+        let distance2 = touchLocation.distance(to: p2Position)
+        
+        // Determine primary and backup casters based on distance
+        let isPlayerOnePrimary = distance1 < distance2
+
+        // Try to cast with primary caster, if fails try backup caster
+        if !castSpell(isPlayerOne: isPlayerOnePrimary, to: touchLocation) {
+            _ = castSpell(isPlayerOne: !isPlayerOnePrimary, to: touchLocation)
+        }
+    }
+    
+    func castSpell(isPlayerOne: Bool, to location: CGPoint) -> Bool {
+        // Check if the wave is active and game is not over
+        guard isSpawningEnabled && !isGameOver else { return false }
+        // Get caster's position
+        let casterPosition = isPlayerOne ? playerView.playerOnePosition : playerView.playerTwoPosition
+        
+        // Get the player's spell
+        let spell = playerState.getSpell(isPlayerOne: isPlayerOne)
+        
+        // Cast the spell
+        let success = spell.cast(from: casterPosition, to: location, by: playerState, isPlayerOne: isPlayerOne, in: self)
+        return success
+    }
+
+    func applySpell(_ spell: Spell, at position: CGPoint) {
+        // Apply spell effects to goblins
+        normalGoblinManager.applySpell(spell, at: position, in: self)
+        largeGoblinManager.applySpell(spell, at: position, in: self)
+        smallGoblinManager.applySpell(spell, at: position, in: self)
+        
+        // Check for potion hits
+        for potion in manaPotions {
+            if position.distance(to: potion.position) <= spell.aoeRadius {
+                handlePotionHit(potion: potion, spellLocation: position)
+            }
+        }
+    }
+    
+    func goblinDied(container: Goblin.GoblinContainer, goblinKilled: Bool) {
+        // Add coins when goblin is killed (50% chance of 5 coins)
+        if goblinKilled {
+            if Bool.random() {
+                playerState.addCoins(5)
+                
+                // Create coin particle effect
+                createCoinEffect(at: container.sprite.position)
+            }
+            // Add points when goblin is eliminated
+            playerState.addScore(points: 10)
+        }
+        
+        // Only decrease if counter is greater than 0
+        if remainingGoblins > 0 {
+            remainingGoblins -= 1
+            updateGoblinCounter()
+            
+            // Check if wave is complete when counter reaches 0
+            if remainingGoblins == 0 {
+                waveCompleted()
+            }
+        }
+    }
+    
+    func waveCompleted() {
+        // Add guard to prevent shop from showing if game is over
+        guard !isGameOver else { return }
+        
+        endWave()
+        showShopView()
+    }
+    
+    func showShopView() {
+        let shopView = ShopView(size: self.size, playerState: playerState) { [weak self] in
+            self?.closeShopView()
+        }
+        shopView.zPosition = 200
+        addChild(shopView)
+        isInShop = true
+    }
+    
+    func closeShopView() {
+        if let shopView = self.children.first(where: { $0 is ShopView }) {
+            shopView.removeFromParent()
+        }
+        isInShop = false
+        startNextWave()
     }
     
     func restartGame() {
@@ -184,17 +459,77 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         physicsWorld.gravity = .zero
         physicsWorld.contactDelegate = self
         
-        // Re-initialize Goblin Manager
-        let goblinProbabilities: [GoblinManager.GoblinType: Double] = [
-            .normal: 70.0,
-            .large: 15.0,
-            .small: 15.0
-        ]
-        goblinManager = GoblinManager(scene: self, probabilities: goblinProbabilities)
+        // Initialize Goblin Managers
+        normalGoblinManager = Goblin(type: .normal, scene: self)
+        largeGoblinManager = Goblin(type: .large, scene: self)
+        smallGoblinManager = Goblin(type: .small, scene: self)
         
         // Start the first wave
         startWave()
     }
     
-    // Rest of your GameScene code...
+    func goToMainMenu() {
+        let mainMenuScene = WGMainMenu(size: self.size)
+        mainMenuScene.scaleMode = SKSceneScaleMode.aspectFill
+        view?.presentScene(mainMenuScene, transition: SKTransition.fade(withDuration: 0.5))
+    }
+    
+    func waveSetup() {
+        waveLabel = SKLabelNode(text: "Wave: \(currentWave)")
+        waveLabel.fontSize = 24
+        waveLabel.fontColor = .black
+        waveLabel.position = CGPoint(x: 80, y: size.height - 60)
+        addChild(waveLabel)
+    }
+    
+    func goblinCounterSetup() {
+        goblinCountLabel = SKLabelNode(text: "Goblins: \(remainingGoblins)")
+        goblinCountLabel.fontSize = 24
+        goblinCountLabel.fontColor = .black
+        goblinCountLabel.position = CGPoint(x: size.width - 100, y: size.height - 60)
+        addChild(goblinCountLabel)
+    }
+    
+    func updateWaveLabel() {
+        waveLabel.text = "Wave: \(currentWave)"
+    }
+    
+    func updateGoblinCounter() {
+        goblinCountLabel.text = "Goblins: \(remainingGoblins)"
+    }
+    
+    func startNextWave() {
+        // Create countdown label
+        let countdownLabel = SKLabelNode(fontNamed: "HelveticaNeue-Bold")
+        countdownLabel.fontSize = 72
+        countdownLabel.position = CGPoint(x: frame.midX, y: frame.midY)
+        countdownLabel.zPosition = 100 // Ensure it appears above other nodes
+        addChild(countdownLabel)
+        
+        // Create countdown sequence
+        var actions: [SKAction] = []
+        
+        // Add actions for each number (5 to 1)
+        for i in (1...5).reversed() {
+            let showNumber = SKAction.run { countdownLabel.text = "\(i)" }
+            let wait = SKAction.wait(forDuration: 1.0)
+            actions.append(contentsOf: [showNumber, wait])
+        }
+        
+        // Add final actions
+        let removeLabel = SKAction.run { countdownLabel.removeFromParent() }
+        let startWaveAction = SKAction.run { [weak self] in
+            guard let self = self else { return }
+            self.currentWave += 1
+            self.updateWaveLabel()
+            self.startWave()
+        }
+        
+        // Add the remove label and start wave actions to the sequence
+        actions.append(removeLabel)
+        actions.append(startWaveAction)
+        
+        // Run the complete sequence
+        run(SKAction.sequence(actions))
+    }
 }
