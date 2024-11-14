cfb6da396be6f338828b2cf02dde5432c8d67d1d
KSandke <kevinsandke7@gmail.com>
2024-11-13 18:08:27 -0800

    Revert "Fix"
    This reverts commit 97d052e0e78e80d7d4bcb00cdd0b00766f5098c4.

--- Code/Models/Goblin.swift
+++ Code/Models/Goblin.swift
@@ -311,23 +311,22 @@ class Goblin {
             removeGoblin(container: container)
         }
     }
-    func handleSpellHit(on goblinNode: SKSpriteNode, damage: CGFloat) {
+    func handleSpellHit(on goblinNode: SKSpriteNode) {
         if let container = goblinContainers.first(where: { $0.sprite == goblinNode }) {
             // Apply damage
-            container.health -= damage
+            container.health -= /* spell damage */
             updateHealthBar(for: container)
 
             // Check if goblin is dead
             if container.health <= 0 {
                 removeGoblin(container: container)
-                // Inform the game scene that a goblin has died
-                if let gameScene = scene as? GameScene {
-                    gameScene.goblinDied(container: container, goblinKilled: true)
-                }
+            // Inform the game scene that a goblin has died
+            if let gameScene = scene as? GameScene {
+                gameScene.goblinDied(container: container, goblinKilled: true)
+            }
             }
         }
     }
-    
     private func updateHealthBar(for container: GoblinContainer) {
         let healthPercentage = container.health / container.maxHealth
         container.healthFill.xScale = max(healthPercentage, 0)
